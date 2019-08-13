56d1b141e1b87b3110630cf6f2e078dd1f19b202
Ali Ahmed <alahmed.se@gmail.com>
2019-08-12 20:51:39 -0700

    Update

--- build.sh
+++ build.sh
@@ -27,11 +27,11 @@ rm -rf pulsar
 git clone -q --depth 1 --branch $GIT_TAG $GIT_REPO
 cd pulsar/pulsar-client-cpp
 
-brew install cmake pkg-config openssl zstd boost boost-python boost-python3 protobuf zlib curl
+brew install cmake pkg-config openssl zstd boost boost-python boost-python3 protobuf zlib curl-openssl
 
 # export LDFLAGS="-L/usr/local/opt/zlib/lib"
 # export CPPFLAGS="-I/usr/local/opt/zlib/include"
-export CMAKE_PREFIX_PATH="/usr/local/opt/zlib:/usr/local/opt/curl"
+export CMAKE_PREFIX_PATH="/usr/local/opt/zlib:/usr/local/opt/curl-openssl"
 
 brew link --force boost
 
